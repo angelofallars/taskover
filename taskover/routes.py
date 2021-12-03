2d1cddc1891a56efecc21ced9ec95ce666f3aee2
Angelo Fallaria <ba.fallaria@gmail.com>
2021-12-03 20:49:43 +0800

    feat: add basic styling

--- taskover/routes.py
+++ taskover/routes.py
@@ -44,7 +44,7 @@ def index():
     else:
         tasks = {}
 
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks, css="index.css")
 
 
 @bp.route("/create", methods=("GET", "POST"))
